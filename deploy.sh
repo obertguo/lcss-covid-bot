04abb3b7a7ff89a62a2646abf846781499df2151
obertguo <obertguo@gmail.com>
2020-11-19 22:58:21 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -e
 git add .
 git commit -m "deploy"
 
-git push git@github.com:obertguo/lcss-covid-bot.git.master
+git push --set-upstream git@github.com:obertguo/lcss-covid-bot.git.master master
